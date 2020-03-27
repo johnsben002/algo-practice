04aa52756fb88e3e11f15208264c01213d32276e
johnsben002 <johnsben002@gmail.com>
2020-03-27 12:32:05 -0400

    stairCombos working with memoization to optimize

---
+++ stairCombos.js
@@ -0,0 +1,23 @@
+// find number of ways you can jump up n stairs in 1, 2, or 3 jumps
+const stairCombos = (n, cache = {}) => {
+  // check cache
+  if (cache[n]) {
+    return cache[n];
+  }
+  
+  // base cases
+  if (n == 0 || n == 1) {
+      return 1;
+  }
+  if (n == 2) {
+    return 2;
+  }
+  
+  // recursive cases
+  const result = stairCombos((n-1), cache) + stairCombos((n-2), cache) + stairCombos((n-3), cache);
+  cache[n] = result;
+  return result;
+    
+};
+
+console.log(stairCombos(60));
