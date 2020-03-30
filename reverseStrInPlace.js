8212bfb81ad556c733f27c8b792ccfa952b2bea3
johnsben002 <johnsben002@gmail.com>
2020-03-30 02:47:24 -0400

    reverse words in place completed

--- reverseStrInPlace.js
+++ reverseStrInPlace.js
@@ -23,3 +23,37 @@ const reverseInPlace = (string) => {
 }
 
 console.log(reverseInPlace(['A','B','C','D','E']));
+
+const reverseWordsInPlace = (message) => {
+
+  // helper function to reverse elements in an array
+  const reverseLoop = (start, end) => {
+    while (start <= end) {
+      [message[start], message[end]] = [message[end], message[start]];
+      start++;
+      end--;
+    }
+  }
+  // reverse all characters
+  reverseLoop(0, message.length-1, message);
+  
+  // iterate through message array to get individual words
+  let wordStarts = 0;
+  for (let i = 0; i <= message.length; i += 1) {
+    // looping until one index past the last one so we can get the ending word
+    if (message[i] === ' ' || message[i] === undefined) {
+      // reverse the word so it's the correct order again
+      reverseLoop(wordStarts, i-1);
+      // set new word starting index
+      wordStarts = i + 1;
+    }
+  };
+  
+  return message;
+};
+
+const message = [ 'c', 'a', 'k', 'e', ' ',
+'p', 'o', 'u', 'n', 'd', ' ',
+'s', 't', 'e', 'a', 'l' ];
+
+console.log(reverseWordsInPlace(message))
