90cc2a1539db31e6f464a4a56c00f4e09f4d36be
johnsben002 <johnsben002@gmail.com>
2020-04-20 17:58:07 -0400

    refactored and cleaned code for eventReveiver

--- eventReceiver.js
+++ eventReceiver.js
@@ -10,37 +10,28 @@ const solution = (messages) => {
     }
 
     trigger() {
-      console.log(this)
       this.messages.forEach(message => this.event(message));
-      console.log(messages)
     }
   }
 
 
   class Receiver {
     constructor() {
-      console.log(this)
       this.messages = [];
-      console.log(messages);
       this.ping = this.ping.bind(this);
     }
 
 
     ping(message) {
-      console.log(this)
       this.messages.push(message);
     }
   }
 
   const myEmitter = new Emitter(messages);
   const myReceiver = new Receiver();
-  console.log(myReceiver)
 
   myEmitter.setEvent(myReceiver.ping);
   myEmitter.trigger();
-
-  myReceiver.ping();
-  console.log(myReceiver)
 
   return myReceiver.messages;
 };
