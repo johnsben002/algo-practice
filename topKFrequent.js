7f3ff2288d400c6fd46b3757b52bd3ea9b21d102
johnsben002 <johnsben002@gmail.com>
2020-05-13 14:06:28 -0400

    added comments to topKFrequent algorithm

--- topKFrequent.js
+++ topKFrequent.js
@@ -1,5 +1,5 @@
 const topKFrequent = (nums, k) => {
-  
+
   // reduce through nums to get tallys of each integer
   const tally = nums.reduce((tally, int) => {
     if (!tally[int]) {
@@ -10,16 +10,19 @@ const topKFrequent = (nums, k) => {
     return tally;
   }, {});
 
+  // initialize an empty array to push in tally obj key-value pairs
   const arr = [];
 
   for (let key in tally) {
     arr.push({key: key, value: tally[key]});
   }
 
+  // sort the array of tally pairs by tally value (largest first)
   arr.sort((a,b) => {
    return b.value - a.value;
   })
 
+  // loop until k and push in integers from sorted tally array
   const result = [];
   for (let i = 0; i < k; i ++) {
     let integer = Number(arr[i].key);
