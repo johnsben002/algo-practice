3070e3cfe79ecd8e21e3d872821a19fa9e5b9c12
johnsben002 <johnsben002@gmail.com>
2020-05-28 14:28:50 -0400

    edge cases handled

--- rottenOrange.js
+++ rottenOrange.js
@@ -10,7 +10,6 @@ Return the minimum number of minutes that must elapse until no cell has a fresh 
  */
 
 const orangesRotting = grid => {
-  console.log(grid);
   // run through the grid and store all the coordinates of the rotten oranges in a queue and the fresh oranges in a set
   // do a BFS on the rotten oranges and take fresh oranges out of the set/include the minute it happened when you put them in the queue
   // once the set is empty, return the largest minute number
@@ -23,16 +22,43 @@ const orangesRotting = grid => {
       if (grid[i][j] === 2) {
         queue.push([i,j,0]);
       } else if (grid[i][j] === 1) {
-        stillFresh.add([i,j]);
+        stillFresh.add(`${i},${j}`);
       }
     }
   }
 
-  console.log(queue);
-  console.log(stillFresh);
+  // initialize counter variable for minutes
+  let minutes = 0;
+
+  while (queue.length) {
+    let [i,j,min] = queue.shift();
+    // increase min counter
+    min++;
+    // check surrounding coordinates
+    for (let [newI, newJ] of [[i, j+1], [i, j-1], [i+1, j], [i-1, j]]) {
+      // make sure coordinates are on the grid
+      if (newI >= 0 && newI < grid.length && newJ >= 0 && newJ < grid[0].length) {
+        // if the orange is fresh, make it rotton and add it's info to the queue
+        if (grid[newI][newJ] === 1) {
+          grid[newI][newJ] = 2;
+          queue.push([newI, newJ, min]);
+          // delete coordinate from stillFresh set
+          stillFresh.delete(`${newI},${newJ}`);
+          // reassign minutes variable if it is not up to date
+          if (minutes < min) {
+            minutes = min;
+          }
+        }
+      }
+    }
+  }
+
+  // if there are still fresh oranges, return -1
+  if (stillFresh.size > 0) return -1;
+
+  // otherwise return minutes
+  return minutes;
 };
-
-
 
 const input = [[2,1,1],[1,1,0],[0,1,1]];
 console.log(orangesRotting(input));
