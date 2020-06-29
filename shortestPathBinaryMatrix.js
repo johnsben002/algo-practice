31ac1670e2d5a15b22ff73dc30a2763241e06b6c
johnsben002 <johnsben002@gmail.com>
2020-06-29 12:37:35 -0500

    optimized solution

--- shortestPathBinaryMatrix.js
+++ shortestPathBinaryMatrix.js
@@ -10,30 +10,27 @@ var shortestPathBinaryMatrix = function(grid) {
     return 1;
   }
 
-  let row, col, step;
-
   // intialize queue for bfs
   const queue = [[0, 0, 1]];
-  // initialize visited set and add starting coordinates
-  const visited = new Set();
-  visited.add('0,0');
+  const possibleMoves = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [0, -1], [1, -1], [1, 0], [1, 1]];
   
   while (queue.length) {
     // dequeue current node
-    [row, col, step] = queue.shift();
+    let [row, col, step] = queue.shift();
     // array of possible moves from current coordinate (8-directional)
-    const possibleMoves = [[row-1, col-1], [row-1, col], [row-1, col+1], [row, col+1], [row, col-1], [row+1, col-1], [row+1, col], [row+1, col+1]];
-    for (let [newRow, newCol] of possibleMoves) {
+    for (let [rowDiff, colDiff] of possibleMoves) {
+      let newRow = row + rowDiff;
+      let newCol = col + colDiff;
       // make sure new coordinates are still on board
       if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length) {
         // if new coordinates are bottom left of grid, increase step and return
         if (newRow === endIndex && newCol === endIndex) {
           return step + 1;
         }
-        // if zero and not visited, add to queue and increase step
-        if (grid[newRow][newCol] === 0 && !visited.has(`${newRow},${newCol}`)) {
-          visited.add(`${newRow},${newCol}`);
+        // if zero, add to queue and increase step
+        if (grid[newRow][newCol] === 0) {
           queue.push([newRow, newCol, step + 1]);
+          grid[newRow][newCol] = 1;
         }
       }
     }
