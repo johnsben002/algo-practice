b38f2d70a43305ca08a6d59dff5960c954f16b5f
johnsben002 <johnsben002@gmail.com>
2020-07-02 15:19:12 -0500

    alternative solution provided

--- surroundedRegions.js
+++ surroundedRegions.js
@@ -10,7 +10,7 @@
  * @param {character[][]} board
  * @return {void} Do not return anything, modify board in-place instead.
  */
-var solve = function(board) {
+var xsolve = function(board) {
     /**
      * @strategy
      * 1. iterate through board and run dfs function on any O that exists and is NOT on a border
@@ -72,7 +72,48 @@ var solve = function(board) {
   }
 };
 
+var solve = function(board) {
+  // mark all O's connected to border as #
+  // left and right borders
+  for (let i = 0; i < board.length; i++) {
+    mark(board, i, 0);
+    mark(board, i, board[0].length-1);
+  }
+  // top and bottom borders
+  for (let i = 1; i < board[0].length-1; i++) {
+    mark(board, 0, i);
+    mark(board, board.length-1, i);
+  }
+
+  // iterate through entire board and change O's to X's and #'s to O's
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] === 'O') {
+        board[i][j] = 'X';
+      }
+      if (board[i][j] === '#') {
+        board[i][j] = 'O';
+      }
+    }
+  }
+};
+
+const mark = (board, i, j) => {
+  // check if on board or not a O
+  if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+  if (board[i][j] !== 'O') return;
+
+  // if it is a 0, mark as #
+  board[i][j] = '#';
+
+  // check all surrounding
+  mark(board, i, j-1);
+  mark(board, i, j+1);
+  mark(board, i-1, j);
+  mark(board, i+1, j);
+}
+
 const input = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]];
 const input2 = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","X","O","X"]];
-console.log(solve(input));
-console.log(solve(input2));
+solve(input);
+solve(input2);
