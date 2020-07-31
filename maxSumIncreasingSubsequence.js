6fdc1a0300f647222d6f5ea4fee9eaddb99071b9
johnsben002 <johnsben002@gmail.com>
2020-07-31 15:09:05 -0500

    cleaned up solution

--- maxSumIncreasingSubsequence.js
+++ maxSumIncreasingSubsequence.js
@@ -4,7 +4,7 @@
  * link to problem: https://www.algoexpert.io/questions/Max%20Sum%20Increasing%20Subsequence
  */
 
-function maxSumIncreasingSubsequence(array) {
+function xmaxSumIncreasingSubsequence(array) {
   // Write your code here.
   // array to keep track of maximum sum up to each index
   const maximums = new Array(array.length);
@@ -47,14 +47,42 @@ function maxSumIncreasingSubsequence(array) {
   return [max, track.reverse()];
 }
 
+function maxSumIncreasingSubsequence(array) {
+  // Write your code here.
+	const sequences = new Array(array.length);
+	const sums = array.map(num => num);
+	let maxSumIndex = 0;
+	for (let i = 0; i < array.length; i++) {
+		const currentNum = array[i];
+		for (let j = 0; j < i; j++) {
+			const otherNum = array[j];
+			if (otherNum < currentNum && sums[j] + currentNum >= sums[i]) {
+				sums[i] = sums[j] + currentNum;
+				sequences[i] = j;
+			}
+		}
+		if (sums[i] >= sums[maxSumIndex]) maxSumIndex = i;
+	}
+	return [sums[maxSumIndex], buildSequence(array, sequences, maxSumIndex)];
+}
 
-// console.log(maxSumIncreasingSubsequence([10, 70, 20, 30, 50, 11, 30])); // [110, [10, 20, 30, 50]]
-// console.log(maxSumIncreasingSubsequence([1]));
-// console.log(maxSumIncreasingSubsequence([-1]));
-// console.log(maxSumIncreasingSubsequence([-1, 1]));
-// console.log(maxSumIncreasingSubsequence([5, 4, 3, 2, 1]));
-// console.log(maxSumIncreasingSubsequence([1, 2, 3, 4, 5]));
-// console.log(maxSumIncreasingSubsequence([-5, -4, -3, -2, -1]));
-// console.log(maxSumIncreasingSubsequence([8, 12, 2, 3, 15, 5, 7]));
-// console.log(maxSumIncreasingSubsequence([10, 15, 4, 5, 11, 14, 31, 25, 31, 23, 25, 31, 50]));
+function buildSequence(array, sequences, currentIndex) {
+	const sequence = [];
+	while (currentIndex !== undefined) {
+		sequence.unshift(array[currentIndex]);
+		currentIndex = sequences[currentIndex];
+	}
+	return sequence;
+}
+
+
+console.log(maxSumIncreasingSubsequence([10, 70, 20, 30, 50, 11, 30])); // [110, [10, 20, 30, 50]]
+console.log(maxSumIncreasingSubsequence([1]));
+console.log(maxSumIncreasingSubsequence([-1]));
+console.log(maxSumIncreasingSubsequence([-1, 1]));
+console.log(maxSumIncreasingSubsequence([5, 4, 3, 2, 1]));
+console.log(maxSumIncreasingSubsequence([1, 2, 3, 4, 5]));
+console.log(maxSumIncreasingSubsequence([-5, -4, -3, -2, -1]));
+console.log(maxSumIncreasingSubsequence([8, 12, 2, 3, 15, 5, 7]));
+console.log(maxSumIncreasingSubsequence([10, 15, 4, 5, 11, 14, 31, 25, 31, 23, 25, 31, 50]));
 console.log(maxSumIncreasingSubsequence([10, 1, 2, 3, 4, 5, 6, 7, 8, 9]));
